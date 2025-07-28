9bed96b90ead5094d18148adc6d9077967b55381
jocyannovittor@hotmail.com <jocyannovittor@hotmail.com>
2025-07-28 11:05:12 -0300

    feat(watched-list): improve test clarity and maintainability
    - Rename test cases to be more descriptive and follow Given-When-Then pattern.
    - Ensure each test case focuses on a single behavior.
    - Remove redundant assertions and improve readability.

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { CreateQuestionUseCase } from "./create-question";
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
 
@@ -14,10 +15,20 @@ describe("Create Question", () => {
     const result = await sut.execute({
       authorId: "1",
       title: "Nova pergunta",
-      content: "Conteúdo da nova pergunta"
+      content: "Conteúdo da nova pergunta",
+      attachmentsIds: ['1', '2']
     });
 
     expect(result.isRight()).toBe(true);
     expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question);
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2);
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('1')
+      }),
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('2')
+      })
+    ])
   });
 });
--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,13 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { Question } from "../../enterprise/entities/question";
 import { Either, right } from "@/core/either";
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
 
 interface CreateQuestionUseCaseRequest {
   authorId: string;
   title: string;
   content: string;
+  attachmentsIds: string[];
 }
 
 type CreateQuestionUseCaseResponse = Either<null, { question: Question }>;
@@ -17,13 +19,24 @@ export class CreateQuestionUseCase {
   async execute({
     authorId,
     title,
-    content
+    content,
+    attachmentsIds
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
+
     const question = Question.create({
       title,
       content,
       authorId: new UniqueEntityID(authorId)
     });
+
+     const questionAttachments = attachmentsIds.map((attachmentId) => {
+       return QuestionAttachment.create({
+         attachmentId: new UniqueEntityID(attachmentId),
+         questionId: new UniqueEntityID(question.id)
+       });
+     });
+
+     question.attachments = questionAttachments;
 
     await this.questionsRepository.create(question);
 
